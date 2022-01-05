511bb98f45c61fdbe72b11721a82425f2efd56cf
Giovane Ferreira da Silva <gfsilva@fcl.com.br>
2022-01-05 18:13:28 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/wordpress/wp-content/themes/allinone/style.css">
+    <title>Allinone</title>
+</head>
+<body>
+    <div class="topo">
+        <nav>
+            <ul>
+                <li><a href="#">Esporte </a></li>
+                <li><a href="#">Música</a></li>
+                <li><a href="#">Filmes</a></li>
+                <li><a href="#">Séries</a></li>
+                <li><a href="#">E-sports</a></li>
+                <li><a href="#">Promoções do dia</a></li>
+            </ul>
+            
+            
+        </nav>
+    </div>
+
+</body>
+</html>
