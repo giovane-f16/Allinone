4864e547fbbe0015b5d8b58fb49dc0575cdb2d30
giovane <giovane_05silva@hotmail.com>
2022-03-18 17:50:35 -0300

    ícone guia

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/wordpress/wp-content/themes/allinone/style.css">
-    <link rel="shortcut icon" type="imagem.png" href="/wordpress/wp-content/themes/allinone/g_logo_shortcute-removebg-preview.png">
+    <link rel="shortcut icon" type="imagem.png" href="/wordpress/wp-content/themes/allinone/g_logo_shortcute.png">
     <title>Allinone</title>
 </head>
 <body>
