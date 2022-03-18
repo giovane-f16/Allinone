9707d485da6fb6eef197b8c1f12839c0dafebee9
giovane <giovane_05silva@hotmail.com>
2022-03-18 17:49:45 -0300

    ícone guia

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/wordpress/wp-content/themes/allinone/style.css">
+    <link rel="shortcut icon" type="imagem.png" href="/wordpress/wp-content/themes/allinone/g_logo_shortcute-removebg-preview.png">
     <title>Allinone</title>
 </head>
 <body>
@@ -20,7 +21,6 @@
         <input type="image" id="menuicon" src="/wordpress/wp-content/themes/allinone/menuicon.png">
         <a href="/wordpress" id="g_logo"><img src="/wordpress/wp-content/themes/allinone/g_logo.png"></a>
         <a href="#" id="searchicon"><img src="/wordpress/wp-content/themes/allinone/searchicon.png"></a>
-        
     </div>
     
     <div id="categorias">
