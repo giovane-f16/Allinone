af2c9b6689c92d9bf71f39ef029ec165454802c1
giovane <giovane_05silva@hotmail.com>
2022-05-07 02:51:13 -0300

    Adicionando shortcut icon na pagina contato

--- page.php
+++ page.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="imagem.png" href="/wordpress/wp-content/themes/allinone/g_logo_shortcute.png">
     <title>Allinone</title>
 </head>
 <body>
